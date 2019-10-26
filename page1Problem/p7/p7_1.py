cf9916fe3b222bcf7a3a49bc14fd3f6263beada7
codelxm123 <32331630+codelxm123@users.noreply.github.com>
2019-10-26 17:44:17 +0800

    Update and rename p7_1.py to page1Problem/p7/p7_1.py

--- page1Problem/p7/p7_1.py
+++ page1Problem/p7/p7_1.py
@@ -6,6 +6,3 @@ class Solution(object):
         """
         x=int(str(x)[::-1]) if x>=0 else -int(str(-x)[::-1])
         return x if x<2147483648 and x>=-2147483648 else 0
-
-x=int(input())
-print(x)
