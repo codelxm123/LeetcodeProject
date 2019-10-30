0c366280d908fabb35ce49ccb6c48b55d73bb53c
codelxm123 <32331630+codelxm123@users.noreply.github.com>
2019-10-30 21:25:58 +0800

    Update p4_1.py

--- page1Problem/p4/p4_1.py
+++ page1Problem/p4/p4_1.py
@@ -1,29 +1,36 @@
+#评论区参考
 class Solution(object):
-    def m_findKth(self,nums1,low1,high1,nums2,low2,high2,k):
-        m1=(low1+high1)//2
-        m2=(low2+high2)//2
-        pass
+    def findKth(self,nums1,st1,nums2,st2,k):
+        if (st1>len(nums1)-1):
+            return nums2[st2+k-1]
+        if (st2>len(nums2)-1):
+            return nums1[st1+k-1]
+        if (k==1):
+            return min(nums1[st1],nums2[st2])
+        mid1=2147483647
+        mid2=2147483647
+        if (st1+k//2-1<len(nums1)):
+            mid1=nums1[st1+k//2-1]
+        if (st2+k//2-1<len(nums2)):
+            mid2=nums2[st2+k//2-1]
+        if (mid1<mid2):
+            return self.findKth(nums1,st1+k//2,nums2,st2,k-k//2)
+        else:
+            return self.findKth(nums1,st1,nums2,st2+k//2,k-k//2)
+
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: float
         """
-        n1=len(nums1)
-        n2=len(nums2)
-        t=[]
-        if (n1>=n2):
-            t=nums1
-            nums1=nums2
-            nums2=t
-        if (n1==0):
-            if (n2%2==0):
-                return (nums2[n2//2]+nums2[n2//2-1])/2
-            else:
-                return nums2[n2//2]
+        if (len(nums1)==0 and len(nums2)==0):
+            return 0.0
+        m=len(nums1)
+        n=len(nums2)
+        l=(m+n+1)//2
+        r=(m+n+2)//2
+        if (l==r):
+            return self.findKth(nums1,0,nums2,0,l)
         else:
-            sumlen=n1+n2
-            ans=self.m_findKth(self,nums1,1,n1,nums2,1,n2,sumlen//2+1)
-            if (sumlen%2==0):
-                ans=(ans+self.m_findKth(self,nums1,1,n1,nums2,1,n2,sumlen//2))/2
-            return ans
+            return (self.findKth(nums1,0,nums2,0,l)+self.findKth(nums1,0,nums2,0,r))/2.0
